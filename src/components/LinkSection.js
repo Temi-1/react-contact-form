984300e13888a5c5c98933f04089a47d4addc854
Temi <temiapantaku@gmail.com>
2022-11-01 07:30:36 +0000

    style webpage

--- src/components/LinkSection.js
+++ src/components/LinkSection.js
@@ -6,8 +6,8 @@ export default function LinkSection() {
         <a id="twitter_link" href="https://twitter.com/theZuriTeam/">Twitter Page</a>
         <a id="btn_zuri" href="https://training.zuri.team/">Zuri Team</a>
         <a id="books" href="http://books.zuri.team/">Zuri Books</a>
-        <a id="book_python" href="https://books.zuri.team/python-for-beginners?ref_id=TemiA">Python for Dummies</a>
-        <a id="pitch" href="https://background.zuri.team">Unearth that coder's past now!</a>
+        <a id="book_python" href="https://books.zuri.team/python-for-beginners?ref_id=TemiA">Python made Easy</a>
+        <a id="pitch" href="https://background.zuri.team">Click to Unearth a coder's past</a>
         <a id="book_design" href="https://books.zuri.team/design-rules">Learn Design for Free!</a>
     </div>
   )
